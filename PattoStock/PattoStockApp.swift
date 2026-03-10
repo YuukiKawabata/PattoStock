f164e43542dc2e3996e6152612e0799df30773a4
YuukiKawabata <kakabata.man@gmail.com>
2026-03-10 21:43:36 +0900

    Initial Commit

---
+++ PattoStock/PattoStockApp.swift
@@ -0,0 +1,17 @@
+//
+//  PattoStockApp.swift
+//  PattoStock
+//
+//  Created by YukiKawabata on 2026/03/10.
+//
+
+import SwiftUI
+
+@main
+struct PattoStockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
